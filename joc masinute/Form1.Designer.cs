ae2c834e3e720eb0ad214f2a846edc5225e5407d
cosminlucian48 <lucian.bejan-topse@s.unibuc.ro>
2021-03-18 15:58:06 +0200

    Final Commit

---
+++ joc masinute/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace joc_masinute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.stanga = new System.Windows.Forms.Timer(this.components);
+            this.dreapta = new System.Windows.Forms.Timer(this.components);
+            this.masina1 = new System.Windows.Forms.Timer(this.components);
+            this.masina2 = new System.Windows.Forms.Timer(this.components);
+            this.masina3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inamic3 = new System.Windows.Forms.PictureBox();
+            this.inamic2 = new System.Windows.Forms.PictureBox();
+            this.inamic1 = new System.Windows.Forms.PictureBox();
+            this.masina = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masina)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // stanga
+            // 
+            this.stanga.Interval = 10;
+            this.stanga.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // dreapta
+            // 
+            this.dreapta.Interval = 10;
+            this.dreapta.Tick += new System.EventHandler(this.dreapta_Tick);
+            // 
+            // masina1
+            // 
+            this.masina1.Enabled = true;
+            this.masina1.Interval = 10;
+            this.masina1.Tick += new System.EventHandler(this.masina1_Tick);
+            // 
+            // masina2
+            // 
+            this.masina2.Enabled = true;
+            this.masina2.Interval = 10;
+            this.masina2.Tick += new System.EventHandler(this.masina2_Tick);
+            // 
+            // masina3
+            // 
+            this.masina3.Enabled = true;
+            this.masina3.Interval = 10;
+            this.masina3.Tick += new System.EventHandler(this.masina3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(181, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Viteza:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 391);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DarkRed;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(52, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 47);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ai pierdut!";
+            this.label5.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Scor";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(56, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // inamic3
+            // 
+            this.inamic3.Image = global::joc_masinute.Properties.Resources.masin3;
+            this.inamic3.Location = new System.Drawing.Point(200, -52);
+            this.inamic3.Name = "inamic3";
+            this.inamic3.Size = new System.Drawing.Size(35, 62);
+            this.inamic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inamic3.TabIndex = 3;
+            this.inamic3.TabStop = false;
+            // 
+            // inamic2
+            // 
+            this.inamic2.Image = global::joc_masinute.Properties.Resources.masina1;
+            this.inamic2.Location = new System.Drawing.Point(106, -52);
+            this.inamic2.Name = "inamic2";
+            this.inamic2.Size = new System.Drawing.Size(35, 62);
+            this.inamic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inamic2.TabIndex = 2;
+            this.inamic2.TabStop = false;
+            // 
+            // inamic1
+            // 
+            this.inamic1.Image = global::joc_masinute.Properties.Resources.masina2;
+            this.inamic1.Location = new System.Drawing.Point(12, -52);
+            this.inamic1.Name = "inamic1";
+            this.inamic1.Size = new System.Drawing.Size(35, 62);
+            this.inamic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inamic1.TabIndex = 1;
+            this.inamic1.TabStop = false;
+            // 
+            // masina
+            // 
+            this.masina.BackColor = System.Drawing.Color.Transparent;
+            this.masina.Image = global::joc_masinute.Properties.Resources.eu;
+            this.masina.Location = new System.Drawing.Point(106, 337);
+            this.masina.Name = "masina";
+            this.masina.Size = new System.Drawing.Size(35, 62);
+            this.masina.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.masina.TabIndex = 0;
+            this.masina.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(264, 411);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inamic3);
+            this.Controls.Add(this.inamic2);
+            this.Controls.Add(this.inamic1);
+            this.Controls.Add(this.masina);
+            this.MaximumSize = new System.Drawing.Size(280, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.inamic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masina)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox masina;
+        private System.Windows.Forms.Timer stanga;
+        private System.Windows.Forms.Timer dreapta;
+        private System.Windows.Forms.PictureBox inamic1;
+        private System.Windows.Forms.Timer masina1;
+        private System.Windows.Forms.PictureBox inamic2;
+        private System.Windows.Forms.PictureBox inamic3;
+        private System.Windows.Forms.Timer masina2;
+        private System.Windows.Forms.Timer masina3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ joc masinute/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace joc_masinute
+{
+    public partial class Form1 : Form
+    {
+        int speed = 3;
+        Label[] drum = new Label[9];
+        int ver=73, orz=-19;
+        int q1, q2, q3;
+        int ok = 0;
+        int scor = 0;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 7; i++)
+            { drum[i].Top += 2;
+                if (drum[i].Top >= this.Height)
+                    drum[i].Top =-drum[i].Height;
+
+                    }
+            scor = scor + speed;
+            label4.Text = Convert.ToString(scor);
+            if (masina.Bounds.IntersectsWith(inamic1.Bounds)|| masina.Bounds.IntersectsWith(inamic2.Bounds)||masina.Bounds.IntersectsWith(inamic3.Bounds))
+                { masina1.Stop();
+                masina2.Stop();
+                masina3.Stop();
+                timer1.Stop();
+                dreapta.Stop();
+                stanga.Stop();
+                label5.Visible = true;
+                label6.Text = Convert.ToString(scor) + " puncte";
+                label6.Visible = true;
+
+                }
+            
+            }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(masina.Location.X>0)
+                 masina.Left -= (speed + 1) / 2;
+        }
+
+        private void dreapta_Tick(object sender, EventArgs e)
+        {
+            if (masina.Location.X <230)
+                masina.Left += (speed+1)/2;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+             dreapta.Start(); 
+            else if (e.KeyCode == Keys.Left)
+                stanga.Start();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            dreapta.Stop();
+            stanga.Stop();
+        }
+
+        private void masina1_Tick(object sender, EventArgs e)
+        {
+          inamic1.Top += speed;
+          
+            
+            // masina2.Start();
+            if (inamic1.Top >= this.Height)
+            {
+                inamic1.Top = -inamic1.Height;
+                int q = random.Next(20, 260);
+                inamic1.Left = q; 
+            }
+            
+
+        }
+
+        private void masina2_Tick(object sender, EventArgs e)
+        {
+            inamic2.Visible = true;
+            inamic2.Top += speed;
+            if (inamic2.Top >= this.Height)
+            {
+                inamic2.Top = -inamic2.Height;
+                int q = random.Next(20, 260);
+                inamic2.Left = q;
+            }
+            
+        }
+
+        private void masina3_Tick(object sender, EventArgs e)
+        {
+            inamic3.Visible = true;
+            inamic3.Top += speed;
+            if (inamic3.Top >= this.Height)
+            {
+                inamic3.Top = -inamic3.Height;
+                int q = random.Next(20, 260);
+                inamic3.Left = q;
+                ok++;
+
+            }
+            if (ok == 2)
+            { viteza(speed, e); scor = scor + speed; ok =0; }
+            
+        }
+        private void viteza(object sender, EventArgs e)
+        {
+            speed++;
+            
+            label1.Text = Convert.ToString(speed);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inamic2.Top = -150;
+            inamic3.Top = -300;
+            label1.Text = Convert.ToString(3);
+            for (int i = 0; i <=7; i++)
+            {
+                drum[i] = new Label();
+                drum[i].BackColor = Color.White;
+                
+                drum[i].Size = new Size(11, 116);
+                if (i == 0) drum[i].Location = new Point(ver, orz);
+                else
+                {
+                    if (i <= 3)
+                        orz = orz + 140;
+                    else
+                    {
+                        if (i == 4) { orz = -19; ver = 189; }
+
+                        else
+                            orz = orz + 140;
+                    }
+                }
+                drum[i].Location = new Point(ver, orz);
+                label4.Text = Convert.ToString(scor);
+                this.Controls.Add(drum[i]);
+            }
+        }
+    }
+}
